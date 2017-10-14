d749c4c4058c15a90e3cd1fd37761d25f6bf7843
Rob Peckham <peckham.rob@gmail.com>
2017-10-14 11:17:54 -0400

    set up most actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,41 @@
 class UsersController < ApplicationController
 		
+    get '/users/:slug' do
+        @user = User.find_by_slug(params[:slug])
+        erb :'/users/show'
+    end
+
+    get '/login' do
+        if !logged_in?
+            erb :'/users/login'
+        else
+            redirect '/cars'
+        end
+    end
+
+    post '/signup' do
+
+    end
+
+    get '/signup' do
+        if !logged_in?
+            erb :'/users/create_user'
+        else
+            redirect '/cars'
+        end
+    end
+
+    post '/signup' do
+
+    end
+
+    get '/logout' do
+        if logged_in?
+            session.destroy
+            redirect '/login'
+        else
+            redirect '/'
+        end
+    end
     
 end
