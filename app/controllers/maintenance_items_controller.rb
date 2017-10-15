650e93ab2aecdda44f80e3357b6dd0268bec0a8e
Rob Peckham <peckham.rob@gmail.com>
2017-10-15 10:42:33 -0400

    added routes

--- app/controllers/maintenance_items_controller.rb
+++ app/controllers/maintenance_items_controller.rb
@@ -8,5 +8,57 @@ class MaintenanceItemsController < ApplicationController
             redirect '/login'
         end
     end
+
+    get '/maintenance_items/new' do  
+        if logged_in?
+            erb :'/maintenance/create_maintenance_item'
+        else
+            redirect '/login'
+        end
+    end
+
+    post '/maintenance_items' do  
+
+    end
+
+    get '/maintenance_items/:id' do  #You might want to make this a slug route
+        if logged_in?
+            @maintenance_item = MaintenanceItem.find_by_id(params[:id])
+            erb :'/maintenance/show_maintenance_item'
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/maintenance_items/:id/edit' do
+        if logged_in?
+            @maintenance_item = MaintenanceItem.find_by_id(params[:id])
+            if @maintenance_item.user_id == current_user.id 
+                erb :'/maintenance/edit_maintenance_item'
+            else
+                redirect '/maintenances_items'
+            end
+        else
+            redirect '/login'
+        end
+    end
+
+    patch '/maintenance_items/:id' do
+
+    end
+
+    delete '/maintenance_items/:id/delete' do
+        if logged_in?
+            @maintenance_item = MaintenanceItem.find_by_id(params[:id])
+            if @maintenance_item.user_id == current_user.id 
+                @maintenance_item.delete
+                redirect '/maintenance_items'
+            else
+                redirect '/maintenance_items'
+            end
+        else
+            redirect '/login'
+        end
+    end
     
 end
