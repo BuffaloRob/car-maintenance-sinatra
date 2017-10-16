54b7a7873e1414536571482811eed3a74655e3b1
BuffaloRob <rob@Robs-MacBook-Air.local>
2017-10-16 14:01:25 -0400

    added post and patch routes

--- app/controllers/maintenance_items_controller.rb
+++ app/controllers/maintenance_items_controller.rb
@@ -18,7 +18,12 @@ class MaintenanceItemsController < ApplicationController
     end
 
     post '/maintenance_items' do  
-
+        if params[:maintenance_name] == ""
+            redirect '/maintenance_items/new'
+        else
+            @maintenance_item = current_user.maintenance_item.create(:name params[:maintenance_name], :mileage_performed params[:mileage_performed], :mileage_due params[:mileage_due], :cost params[:cost])
+            redirect "/maintenance_items/#{@maintenance_item.id}"
+        end
     end
 
     get '/maintenance_items/:id' do  #You might want to make this a slug route
@@ -44,7 +49,17 @@ class MaintenanceItemsController < ApplicationController
     end
 
     patch '/maintenance_items/:id' do
-
+        if params[:maintenance_name] == ""
+            redirect "/maintenance_items/#{params[:id]}/edit"
+        else
+            @maintenance_item = MaintenanceItem.find_by_id(params[:id])
+            @maintenance_item.name = params[:maintenance_name]
+            @maintenance_item.mileage_performed = params[:mileage_performed]
+            @maintenance_item.mileage_due = params[:mileage_due]
+            @maintenance_item.cost = params[:cost]
+            @maintenance_item.save
+            redirect "/maintenance_items/#{@maintenance_item.id}"
+        end
     end
 
     delete '/maintenance_items/:id/delete' do
