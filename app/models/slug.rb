7dcf8ec4758ce8c7f624e006cb2d148f652a4ef2
BuffaloRob <rob@Robs-MacBook-Air.local>
2017-10-17 14:06:03 -0400

    Made slug a module

---
+++ app/models/slug.rb
@@ -0,0 +1,12 @@
+module Slug
+  module InstanceMethods
+    def slug 
+      name.downcase.gsub(" ", "-")
+    end
+  end
+  module ClassMethods
+    def find_by_slug(slug)
+      self.all.find{|model| model.slug == slug}
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :cars
+  include Slug::InstanceMethods
+  extend Slug::ClassMethods
 
-  def slug
-    name.downcase.gsub(" ", "-")
-  end
-
-  def self.find_by_slug(slug)
-    User.all.find{|user| user.slug == slug}
-  end
 end
