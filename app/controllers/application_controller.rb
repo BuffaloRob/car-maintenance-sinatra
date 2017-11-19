1571387cd77d8efdbc56ecf4a57bbd22dd11b3e8
Rob Peckham <peckham.rob@gmail.com>
2017-11-19 12:29:41 -0500

    CSS stylesheet working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
   enable :sessions
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
-  set :public_folder, 'public'
+  set :public_folder, Proc.new { File.join(root, "../public/") }
 
   get '/' do
     erb :'/index'
