ce152f5645501a69db3480de97a68ba606f90e77
Rob Peckham <peckham.rob@gmail.com>
2017-11-25 16:50:27 -0500

    found workaround for problem with car_maint_item using same params[:id] as maint_item

--- app/controllers/car_maintenance_items_controller.rb
+++ app/controllers/car_maintenance_items_controller.rb
@@ -32,9 +32,11 @@ class CarMaintenanceItemsController < ApplicationController
 
     get '/car_maintenance_items/:id' do  #You might want to make this a slug route
         if logged_in?
-            @maintenance_item = MaintenanceItem.find_by_id(params[:id])
+            # @maintenance_item = MaintenanceItem.find_by_id(params[:id]) #can't use the same param as below
             @car_maintenance_item = CarMaintenanceItem.find_by_id(params[:id])
-            # binding.pry
+            # @maintenance_id = @car_maintenance_item.maintenance_item_id
+            @maintenance_item = MaintenanceItem.find_by_id(@car_maintenance_item.maintenance_item_id)
+            binding.pry
             erb :'/car_maintenance/show_car_maintenance_item'
         else
             redirect '/login'
@@ -45,8 +47,8 @@ class CarMaintenanceItemsController < ApplicationController
         if logged_in?
             # binding.pry
             @cars = current_user.cars
-            @maintenance_item = MaintenanceItem.find_by_id(params[:id])
             @car_maintenance_item = CarMaintenanceItem.find_by_id(params[:id])
+            @maintenance_item = MaintenanceItem.find_by_id(@car_maintenance_item.maintenance_item_id)
             erb :'/car_maintenance/edit_car_maintenance_item'
             #### DO I NEED TO VALIDATE HERE???? Need to do it a different way, similar to the 'post "/maintenance_items"' route
             # if @maintenance_item.user_id == current_user.id 
@@ -63,15 +65,12 @@ class CarMaintenanceItemsController < ApplicationController
         if params[:maintenance_name] == ""
             redirect "/car_maintenance_items/#{params[:id]}/edit"
         else
-            
             @car = Car.find_by_id(params[:car_id])
-            #Need to change car.id to new id
-            @maintenance_item = MaintenanceItem.find_by_id(params[:id])
-            @maintenance_item.name = params[:maintenance_name]
             @car_maintenance_item = CarMaintenanceItem.find_by_id(params[:id])
             @car_maintenance_item.mileage_performed = params[:mileage_performed]
             @car_maintenance_item.mileage_due = params[:mileage_due]
             @car_maintenance_item.cost = params[:cost]
+            @maintenance_item = MaintenanceItem.find_by_id(@car_maintenance_item.maintenance_item_id)
             # @car.maintenance_items = @maintenance_item
             # binding.pry
             @maintenance_item.save
@@ -83,10 +82,10 @@ class CarMaintenanceItemsController < ApplicationController
 
     delete '/car_maintenance_items/:id/delete' do
         if logged_in?
-            @maintenance_item = MaintenanceItem.find_by_id(params[:id])
             @car_maintenance_item = CarMaintenanceItem.find_by_id(params[:id])
+            # @maintenance_item = MaintenanceItem.find_by_id(params[:id])
+            # @maintenance_item.delete
             @car_maintenance_item.delete
-            @maintenance_item.delete
             redirect '/car_maintenance_items'
             ####Same issue as line 46
             # if @maintenance_item.user_id == current_user.id 
