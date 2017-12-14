17acf9421840fd2586bbfc9709e962b529156922
BuffaloRob <rob@Robs-MacBook-Air.local>
2017-12-14 09:21:53 -0500

    commented out AR validations

--- app/models/car_maintenance_item.rb
+++ app/models/car_maintenance_item.rb
@@ -2,6 +2,8 @@ class CarMaintenanceItem < ActiveRecord::Base
   belongs_to :car
   belongs_to :maintenance_item
 
-  validates :mileage_performed, :numericality => {:only_integer => true}
+  # validates :mileage_performed, :numericality => {:only_integer => true}
+  # validates :mileage_due, :numericality => {:only_integer => true}
+  # validates :cost, :numericality => {:only_integer => true}
   
 end
