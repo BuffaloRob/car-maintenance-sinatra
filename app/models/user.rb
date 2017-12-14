7491892ece7304f9eec72e8a54c8ad8f7a76cfaa
Rob <github email address>
2017-12-14 07:15:05 -0500

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :cars
   has_many :maintenance_items
+
+  validates :username, uniqueness: true#, message: "Please choose a different name"
+  validates :email, uniqueness: true#, message: "That email is already being used"
+
   include Slug::InstanceMethods
   extend Slug::ClassMethods
 
